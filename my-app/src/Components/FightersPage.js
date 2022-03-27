22af04615ca2888960173d69266810e6f3910b14
Jake <prevou15@gmail.com>
2022-03-27 11:22:14 -0500

    Moving GET req/fighters state to FightersPage

--- my-app/src/Components/FightersPage.js
+++ my-app/src/Components/FightersPage.js
@@ -1,10 +1,21 @@
 import React from "react";
 import FightersList from "./FightersList";
 
-function FightersPage({ fighters}) {
+function FightersPage() {
+    const [fighters, setFighters] = useState([]);
+  
+    function handleAddFighter(newFighter) {
+      setFighters([...fighters, newFighter]);
+    }
+  
+    useEffect(() => {
+      fetch("http://localhost:3000/fighters")
+        .then((r) => r.json())
+        .then((fighters) => setFighters(fighters));
+    }, []);
   return (
     <header>
-        
+        <FightersForm onAddFighter={handleAddFighter} /> 
         <FightersList fighters={fighters} />
     </header>
   );

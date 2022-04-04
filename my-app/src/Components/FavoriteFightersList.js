8817d509b3a9c69436812b76f7e891897618a7d0
Jake <prevou15@gmail.com>
2022-04-04 04:21:48 -0500

    Passing down onDeleteFighter to FighterCard for DELETE request

--- my-app/src/Components/FavoriteFightersList.js
+++ my-app/src/Components/FavoriteFightersList.js
@@ -1,13 +1,16 @@
 import React from "react";
 import FighterCard from "./FighterCard";
 
-function FavoriteFightersList({ fighters }) {
+function FavoriteFightersList({ fighters, onUpdateFighter, onDeleteFighter }) {
 
 
   return (
-    <ul className="cards">{fighters.map((fighter) => (
-      <FighterCard key={fighter.id} fighter={fighter} />
-    ))}</ul>
+    <div className="ffbox">
+      <ul className="ffcards">{fighters.map((fighter) => (
+        <FighterCard key={fighter.id} fighter={fighter} onUpdateFighter={onUpdateFighter} onDeleteFighter={onDeleteFighter} />
+      ))}
+      </ul>
+    </div>
   );
 }
 
